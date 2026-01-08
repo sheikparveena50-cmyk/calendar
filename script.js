63c4e5d4dbb8211877c4f3a6493e528c41c40e7e
sheikparveena50-cmyk <sheikparveena50@gmail.com>
2026-01-08 15:15:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,20 @@ const monthYearEl = document.getElementById("monthYear");
 
 let date = new Date();
 
+/* 🎉 FESTIVALS LIST */
+const festivals = {
+  "1-1": "New Year 🎊",
+  "1-14": "Sankranti 🪁",
+  "1-26": "Republic Day 🇮🇳",
+  "3-29": "Ugadi 🌸",
+  "4-14": "Ambedkar Jayanti",
+  "8-15": "Independence Day 🇮🇳",
+  "10-2": "Gandhi Jayanti",
+  "10-12": "Dussehra 🏹",
+  "11-1": "Diwali 🪔",
+  "12-25": "Christmas 🎄"
+};
+
 function renderCalendar() {
   daysEl.innerHTML = "";
   const year = date.getFullYear();
@@ -20,7 +34,14 @@ function renderCalendar() {
 
   for (let i = 1; i <= lastDate; i++) {
     const day = document.createElement("div");
-    day.innerText = i;
+    day.innerHTML = `<strong>${i}</strong>`;
+
+    const key = `${month + 1}-${i}`;
+
+    if (festivals[key]) {
+      day.innerHTML += `<div class="festival">${festivals[key]}</div>`;
+      day.classList.add("festival-day");
+    }
 
     if (
       i === new Date().getDate() &&
